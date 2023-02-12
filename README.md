0590518fe56d72b03df110d8f14c36962c37a84f
saw-your-packet <saw-your-packet@breakingbreakpoints.com>
2023-02-12 14:58:24 +0200

    added necessary txt files and updated setup.py

---
+++ README.md
@@ -0,0 +1,15 @@
+# EC2StepShell
+
+EC2StepShell is an AWS post-exploitation tool for getting reverse shells in public or private EC2 instances
+
+## Usage
+
+```bash
+EC2StepShell -h
+```
+
+## Installation
+
+```bash
+python -m pip install EC2StepShell
+```
--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Information Technology',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Security'
+]
+
 
 setup(
     name ='EC2StepShell',
-    version ='0.1.0',
-    description ='EC2StepShell is an AWS post-exploitation tool for getting reverse shells in public or private EC2 instances.',
-    author = 'Eduard Agavriloae'
+    version ='0.0.1',
+    description ='EC2StepShell is an AWS post-exploitation tool for getting reverse shells in public or private EC2 instances',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    author = 'Eduard Agavriloae',
+    author_email='saw-your-packet@breakingbreakpoints.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='EC2StepShell',
+    packages=find_packages(),
+    requires=['colorama', 'termcolor', 'pyfiglet', 'boto3'],
+    url='https://github.com/saw-your-packet/EC2StepShell',
+    py_modules=['ec2stepshell']
 )
